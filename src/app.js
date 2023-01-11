f277771fe8c20cba4124ecfec5101ff59154c364
Andy2020lv <andyhernandez5102@gmail.com>
2023-01-11 08:46:58 -0400

    Added interaction with the DOM, and validation checks

---
+++ src/app.js
@@ -0,0 +1,99 @@
+//autobind decorator. "PropertyDescriptor" returns "undefined"
+function validate(validatableInput) {
+    var isValid = true;
+    if (validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if (validatableInput.minLength != undefined &&
+        typeof validatableInput.value === "string") {
+        isValid =
+            isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+    if (validatableInput.maxLength != undefined &&
+        typeof validatableInput.value === "string") {
+        isValid =
+            isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+    if (validatableInput.min != null &&
+        typeof validatableInput.value === "number") {
+        isValid = isValid && validatableInput.value >= validatableInput.min;
+    }
+    if (validatableInput.max != null &&
+        typeof validatableInput.value === "number") {
+        isValid = isValid && validatableInput.value <= validatableInput.max;
+    }
+    return isValid;
+}
+var ProjectInput = /** @class */ (function () {
+    function ProjectInput() {
+        this.templateElement = document.getElementById("project-input");
+        this.hostElement = document.getElementById("app");
+        var importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = "user-input";
+        this.titleInputElement = this.element.querySelector("#title");
+        this.descriptionInputElement = this.element.querySelector("#description");
+        this.peopleInputElement = this.element.querySelector("#people");
+        this.configure();
+        this.attach();
+    }
+    ProjectInput.prototype.gatherUserInput = function () {
+        var enteredTitle = this.titleInputElement.value;
+        var enteredDescription = this.descriptionInputElement.value;
+        var enteredPeople = this.peopleInputElement.value;
+        //
+        var titleValidatable = {
+            value: enteredTitle,
+            required: true
+        };
+        var descriptionValidatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5
+        };
+        var peopleValidatable = {
+            value: +enteredPeople,
+            required: true,
+            min: 1,
+            max: 5
+        };
+        //Check if any of the fields are empty
+        if (!validate(titleValidatable) ||
+            !validate(descriptionValidatable) ||
+            !validate(peopleValidatable)
+        //   enteredTitle.trim().length === 0 ||
+        //   enteredDescription.trim().length === 0 ||
+        //   enteredPeople.trim().length === 0
+        ) {
+            alert("Invalid input, please try again");
+            return;
+        }
+        else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    };
+    ProjectInput.prototype.clearInputs = function () {
+        this.titleInputElement.value = "";
+        this.descriptionInputElement.value = "";
+        this.peopleInputElement.value = "";
+    };
+    //   @autobind
+    ProjectInput.prototype.sumbmitHandler = function (event) {
+        event.preventDefault();
+        var userInput = this.gatherUserInput();
+        //Check if it returned a tuple (a fixed size list)
+        if (Array.isArray(userInput)) {
+            var title = userInput[0], desc = userInput[1], people = userInput[2];
+            console.log(title, desc, people);
+            this.clearInputs();
+        }
+    };
+    ProjectInput.prototype.configure = function () {
+        this.element.addEventListener("submit", this.sumbmitHandler.bind(this));
+    };
+    ProjectInput.prototype.attach = function () {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    };
+    return ProjectInput;
+}());
+var prjInput = new ProjectInput();
