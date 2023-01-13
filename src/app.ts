48af565ebb390a28ac93db51d28ab3dda7865269
Andy2020lv <andyhernandez5102@gmail.com>
2023-01-13 12:15:37 -0400

    Now work with es6 imports

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,6 @@
-//autobind decorator. "PropertyDescriptor" returns "undefined"
+import { ProjectInput } from "./components/project-input.js";
+import { ProjectList } from "./components/project-list.js";
 
-// function autobind(
-//   _target: object,
-//   _propertyKey: string | symbol,
-//   descriptor: TypedPropertyDescriptor<any>
-// ) {
-//   const originalMethod = descriptor.value;
-//   const adjDescriptor: PropertyDescriptor = {
-//     configurable: true,
-//     get() {
-//       const boundFn = originalMethod.bind(this);
-//       return boundFn;
-//     },
-//   };
-//   return adjDescriptor;
-// }
-
-// Drag and Drop interfaces
-
-// This is special syntax from TS
-
-/// <reference path="components/project-list.ts"/>
-/// <reference path="components/project-input.ts"/>
-namespace App {
-  new ProjectInput();
-  new ProjectList("active");
-  new ProjectList("finished");
-}
+new ProjectInput();
+new ProjectList("active");
+new ProjectList("finished");
